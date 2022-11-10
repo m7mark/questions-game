2a40549eb94b77071b6e2ec116355789ecd6de6c
Mark <m7mark@yandex.ru>
2022-11-10 03:36:25 +0300

    add layout to app

--- src/components/screens/Questions/Questions.tsx
+++ src/components/screens/Questions/Questions.tsx
@@ -13,20 +13,18 @@ export const Questions: FC = () => {
   const { num, countElements, currentQuestion, setQuestion } = useQuestions()
   const { push } = useRouter()
   return (
-    <Layout>
-      <div className={styles.questions}>
-        <FaArrowCircleLeft className={styles.back} onClick={() => push('/')} />
-        <div className={styles.counter}>{`${num}/${countElements}`}</div>
-        <h2>{currentQuestion}</h2>
-        <div className={styles.buttons}>
-          <button onClick={() => setQuestion('prev')}>
-            <FaChevronLeft />
-          </button>
-          <button onClick={() => setQuestion('next')}>
-            <FaChevronRight />
-          </button>
-        </div>
+    <div className={styles.questions}>
+      <FaArrowCircleLeft className={styles.back} onClick={() => push('/')} />
+      <div className={styles.counter}>{`${num}/${countElements}`}</div>
+      <h2>{currentQuestion}</h2>
+      <div className={styles.buttons}>
+        <button onClick={() => setQuestion('prev')}>
+          <FaChevronLeft />
+        </button>
+        <button onClick={() => setQuestion('next')}>
+          <FaChevronRight />
+        </button>
       </div>
-    </Layout>
+    </div>
   )
 }
--- src/components/screens/Start/Start.tsx
+++ src/components/screens/Start/Start.tsx
@@ -9,18 +9,16 @@ import { FaPlayCircle } from 'react-icons/fa'
 export const Start: FC = () => {
   const router = useRouter()
   return (
-    <Layout>
-      <div className={styles.start}>
-        <Image alt="friends" src={friends} className={styles.friends} />
-        <div className={styles.title}>
-          <h2>Самые</h2>
-          <h2 style={{ backgroundColor: 'red' }}>интересные</h2>
-          <h2>вопросы</h2>
-        </div>
-        <button onClick={() => router.push('questions')}>
-          <FaPlayCircle />
-        </button>
+    <div className={styles.start}>
+      <Image alt="friends" src={friends} className={styles.friends} />
+      <div className={styles.title}>
+        <h2>Самые</h2>
+        <h2 style={{ backgroundColor: 'red' }}>интересные</h2>
+        <h2>вопросы</h2>
       </div>
-    </Layout>
+      <button onClick={() => router.push('questions')}>
+        <FaPlayCircle />
+      </button>
+    </div>
   )
 }
