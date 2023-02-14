c76561dde6d82dad97f253fc72b654c229f2514a
Mark <m7mark@yandex.ru>
2023-02-14 21:09:20 +0300

    add new font

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,7 +7,7 @@ export default function Document() {
         <meta charSet="UTF-8" />
         <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
         <link
-          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;500;600;700&display=swap"
           rel="stylesheet"
         />
       </Head>
