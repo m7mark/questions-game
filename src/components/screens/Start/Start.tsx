a9bd885cf09f17274385bf740d540992d3addc64
Mark <m7mark@yandex.ru>
2023-02-26 02:39:54 +0300

    add framer motion

--- src/components/screens/Start/Start.tsx
+++ src/components/screens/Start/Start.tsx
@@ -1,13 +1,15 @@
 import clsx from 'clsx'
+import { motion } from 'framer-motion'
 import { useRouter } from 'next/router'
 import { useState } from 'react'
 import { FaInfo } from 'react-icons/fa'
-import { CSSTransition } from 'react-transition-group'
 import FriendsImage from '../../../assets/imgs/friends.svg'
 import { useKeyPress } from '../../../hooks/useKeys'
 import { Toggle as ToggleTheme } from '../../ui'
 import { Info } from './Info/Info'
 import styles from './Start.module.scss'
+
+import { AnimatePresence } from 'framer-motion'
 
 export const Start = () => {
   const { push } = useRouter()
@@ -25,16 +27,19 @@ export const Start = () => {
       >
         <FaInfo className="text-gray-900 dark:text-gray-200" />
       </button>
-      <CSSTransition
-        in={showInfo}
-        timeout={300}
-        classNames="slide-animation"
-        unmountOnExit
-      >
-        <div className={styles.infoWrapper}>
-          <Info close={() => setShowInfo(false)} />
-        </div>
-      </CSSTransition>
+      <AnimatePresence>
+        {showInfo && (
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            className={styles.infoWrapper}
+          >
+            <Info close={() => setShowInfo(false)} />
+          </motion.div>
+        )}
+      </AnimatePresence>
+
       <ToggleTheme className={styles.toggle} />
       <FriendsImage draggable={false} className="sm:hidden mt-4" />
       <div className="text-black dark:text-white">
